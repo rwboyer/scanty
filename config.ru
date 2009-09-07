294636227c5f583ddc6bdffad5a625fcdcb475b9
rwboyer <rwboyer@mac.com>
2009-09-07 17:49:45 -0400

    Sinatra:Base subclassed version working

--- config.ru
+++ config.ru
@@ -1,11 +1,8 @@
 require 'rubygems'
-require 'sinatra'
-
-Sinatra::Application.default_options.merge!(
-  :views => File.join(File.dirname(__FILE__), 'views'),
-  :run => false,
-  :env => ENV['RACK_ENV']
-)
+require 'sinatra/base'
 
 require 'main'
-run Sinatra.application
+
+use Rack::Static, :urls => ["/css", "/images"], :root => "public"
+
+run Scanty
--- main.rb
+++ main.rb
@@ -1,125 +1,136 @@
 require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
 
 $LOAD_PATH.unshift File.dirname(__FILE__) + '/vendor/sequel'
+
 require 'sequel'
+require 'ostruct'
 
-configure do
-	Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://blog.db')
+Blog = OpenStruct.new(
+	:title => 'a scanty blog',
+	:author => 'John Doe',
+	:url_base => 'http://localhost:9292/',
+	:admin_password => 'test',
+	:admin_cookie_key => 'test',
+	:admin_cookie_value => '51d6d976913ace58',
+	:disqus_shortname => nil
+)
 
-	require 'ostruct'
-	Blog = OpenStruct.new(
-		:title => 'a scanty blog',
-		:author => 'John Doe',
-		:url_base => 'http://localhost:4567/',
-		:admin_password => 'changeme',
-		:admin_cookie_key => 'scanty_admin',
-		:admin_cookie_value => '51d6d976913ace58',
-		:disqus_shortname => nil
-	)
-end
+class Scanty < Sinatra::Base
 
-error do
-	e = request.env['sinatra.error']
-	puts e.to_s
-	puts e.backtrace.join("\n")
-	"Application error"
-end
+	configure do
+		set :views, File.join(File.dirname(__FILE__), 'views')
+		set :run, false
+		set :env, ENV['RACK_ENV']
 
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib')
-require 'post'
+		Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://blog.db')
 
-helpers do
-	def admin?
-		request.cookies[Blog.admin_cookie_key] == Blog.admin_cookie_value
 	end
 
-	def auth
-		stop [ 401, 'Not authorized' ] unless admin?
+	error do
+		e = request.env['sinatra.error']
+		puts e.to_s
+		puts e.backtrace.join("\n")
+		"Application error"
 	end
-end
 
-layout 'layout'
+	$LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib')
+	require 'post'
 
-### Public
+	helpers do
+		def admin?
+			request.cookies[Blog.admin_cookie_key] == Blog.admin_cookie_value
+		end
 
-get '/' do
-	posts = Post.reverse_order(:created_at).limit(10)
-	erb :index, :locals => { :posts => posts }, :layout => false
-end
+		def auth
+			stop [ 401, 'Not authorized' ] unless admin?
+		end
+	end
 
-get '/past/:year/:month/:day/:slug/' do
-	post = Post.filter(:slug => params[:slug]).first
-	stop [ 404, "Page not found" ] unless post
-	@title = post.title
-	erb :post, :locals => { :post => post }
-end
+	layout 'layout'
 
-get '/past/:year/:month/:day/:slug' do
-	redirect "/past/#{params[:year]}/#{params[:month]}/#{params[:day]}/#{params[:slug]}/", 301
-end
+	### Public
 
-get '/past' do
-	posts = Post.reverse_order(:created_at)
-	@title = "Archive"
-	erb :archive, :locals => { :posts => posts }
-end
+	get '/' do
+		posts = Post.reverse_order(:created_at).limit(10)
+		erb :index, :locals => { :posts => posts }, :layout => false
+	end
 
-get '/past/tags/:tag' do
-	tag = params[:tag]
-	posts = Post.filter(:tags.like("%#{tag}%")).reverse_order(:created_at).limit(30)
-	@title = "Posts tagged #{tag}"
-	erb :tagged, :locals => { :posts => posts, :tag => tag }
-end
+	get '/past/:year/:month/:day/:slug/' do
+		post = Post.filter(:slug => params[:slug]).first
+		stop [ 404, "Page not found" ] unless post
+		@title = post.title
+		erb :post, :locals => { :post => post }
+	end
 
-get '/feed' do
-	@posts = Post.reverse_order(:created_at).limit(20)
-	content_type 'application/atom+xml', :charset => 'utf-8'
-	builder :feed
-end
+	get '/past/:year/:month/:day/:slug' do
+		redirect "/past/#{params[:year]}/#{params[:month]}/#{params[:day]}/#{params[:slug]}/", 301
+	end
 
-get '/rss' do
-	redirect '/feed', 301
-end
+	get '/past' do
+		posts = Post.reverse_order(:created_at)
+		@title = "Archive"
+		erb :archive, :locals => { :posts => posts }
+	end
 
-### Admin
+	get '/past/tags/:tag' do
+		tag = params[:tag]
+		posts = Post.filter(:tags.like("%#{tag}%")).reverse_order(:created_at).limit(30)
+		@title = "Posts tagged #{tag}"
+		erb :tagged, :locals => { :posts => posts, :tag => tag }
+	end
 
-get '/auth' do
-	erb :auth
-end
 
-post '/auth' do
-	set_cookie(Blog.admin_cookie_key, Blog.admin_cookie_value) if params[:password] == Blog.admin_password
-	redirect '/'
-end
+	get '/feed' do
+		@posts = Post.reverse_order(:created_at).limit(20)
+		content_type 'application/atom+xml', :charset => 'utf-8'
+		builder :feed
+	end
 
-get '/posts/new' do
-	auth
-	erb :edit, :locals => { :post => Post.new, :url => '/posts' }
-end
+	get '/rss' do
+		redirect '/feed', 301
+	end
 
-post '/posts' do
-	auth
-	post = Post.new :title => params[:title], :tags => params[:tags], :body => params[:body], :created_at => Time.now, :slug => Post.make_slug(params[:title])
-	post.save
-	redirect post.url
-end
+	### Admin
 
-get '/past/:year/:month/:day/:slug/edit' do
-	auth
-	post = Post.filter(:slug => params[:slug]).first
-	stop [ 404, "Page not found" ] unless post
-	erb :edit, :locals => { :post => post, :url => post.url }
-end
+	get '/auth' do
+		erb :auth
+	end
 
-post '/past/:year/:month/:day/:slug/' do
-	auth
-	post = Post.filter(:slug => params[:slug]).first
-	stop [ 404, "Page not found" ] unless post
-	post.title = params[:title]
-	post.tags = params[:tags]
-	post.body = params[:body]
-	post.save
-	redirect post.url
+	post '/auth' do
+		response.set_cookie(Blog.admin_cookie_key, Blog.admin_cookie_value) if params[:password] == Blog.admin_password
+		redirect '/'
+	end
+
+	get '/posts/new' do
+		auth
+		erb :edit, :locals => { :post => Post.new, :url => '/posts' }
+	end
+
+	post '/posts' do
+		auth
+		post = Post.new :title => params[:title], :tags => params[:tags], :body => params[:body], :created_at => Time.now, :slug => Post.make_slug(params[:title])
+		post.save
+		redirect post.url
+	end
+
+	get '/past/:year/:month/:day/:slug/edit' do
+		auth
+		post = Post.filter(:slug => params[:slug]).first
+		stop [ 404, "Page not found" ] unless post
+		erb :edit, :locals => { :post => post, :url => post.url }
+	end
+
+	post '/past/:year/:month/:day/:slug/' do
+		auth
+		post = Post.filter(:slug => params[:slug]).first
+		stop [ 404, "Page not found" ] unless post
+		post.title = params[:title]
+		post.tags = params[:tags]
+		post.body = params[:body]
+		post.save
+		redirect post.url
+	end
+
 end
 
